5a48a73d58376c724384ed594b89d0c9d533c310
kulturman <arnaudbakyono@gmail.com>
2025-11-29 15:01:32 +0000

    Add database

---
+++ src/main/java/com/kulturman/irembotest/IremboTestApplication.java
@@ -0,0 +1,13 @@
+package com.kulturman.irembotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IremboTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IremboTestApplication.class, args);
+	}
+
+}
